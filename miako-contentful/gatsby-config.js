37b911dff0313e6738e87a530313bf81d78afe51
rizsoo <89772889+rizsoo@users.noreply.github.com>
2023-04-16 20:43:08 +0200

    basis

---
+++ miako-contentful/gatsby-config.js
@@ -0,0 +1,43 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `z54h1ca29glj`,
+        accessToken: process.env.CONTENTFUL_CONTENT_DELIVERY_API,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `#000`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/assets/miako_fav.png',
+      },
+    },
+    `gatsby-plugin-image`,
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-recaptcha`,
+    `gatsby-plugin-sitemap`,
+  ],
+  siteMetadata: {
+    title: `Mi a Kő Étterem`,
+    description: `Káli-medencei Borok Háza`,
+    image: `/src/assets/miako_fav.png`,
+    siteUrl: `https://www.mia-ko.hu`,
+  },
+}
